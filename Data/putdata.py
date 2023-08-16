2e398db8d063232a125d642ad625563c8687a389
CDYJang <139101296+CDYJang@users.noreply.github.com>
2023-08-16 17:22:33 +0900

    db put

---
+++ Data/putdata.py
@@ -0,0 +1,12 @@
+import df2sql
+
+# MySQL 연결 정보 설정
+hostname='cdymysql.mysql.database.azure.com'
+username='cdy'
+password='Pa12341234!@#$'
+database='testdb'
+
+# MySQL 데이터베이스 연결
+connection = pymysql.connect(user=username, password=password, host=hostname, port=3306, database=database, ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
+# 커서 생성
+cursor = connection.cursor()
