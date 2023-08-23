8e8bcb83e897c26f55c6fceb278fb30a99309b51
bmfe88 <140584400+bmfe88@users.noreply.github.com>
2023-08-23 09:59:04 +0900

    Add files via upload

--- Front/project6/main.js
+++ Front/project6/main.js
@@ -20,3 +20,51 @@ window.onload=()=>{
         document.querySelector('.header-1').classList.remove('active');
     } 
 }
+
+/*var swiper =new Swiper(".gear",{
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+},
+breakpoints: {
+    0:{
+        slidesPerView:1,
+    },
+    768:{
+        slidesPerView:2,
+    },
+    1024:{
+        slidesPerView:3,
+    },
+},
+});
+
+var swiper =new Swiper(".slider",{
+    spaceBetween:10,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+},
+nevigation:{
+    nextE1:".swiper-button-next",
+    prevE1:".swiper-button-prev",
+},
+breakpoints: {
+    0:{
+        slidesPerView:1,
+    },
+    450:{
+        slidesPerView:2,
+    },
+    768:{
+        slidesPerView:3,
+    },
+    1024:{
+        slidesPerView:4,
+    },
+},
+});*/
